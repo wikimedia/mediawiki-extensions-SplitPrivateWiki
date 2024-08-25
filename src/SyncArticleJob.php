210895bacce442dba08b01c4a11c19e1244a6aa4
Umherirrender <umherirrender_de.wp@web.de>
2024-08-25 22:36:16 +0200

    Use DB_PRIMARY instead of deprecated DB_MASTER
    Raise support floor to 1.35.3 for this.
    
    Change-Id: Ic2cceb47fee90d736737e07ef085d124cc1b497e

--- src/SyncArticleJob.php
+++ src/SyncArticleJob.php
@@ -72,7 +72,7 @@ class SyncArticleJob extends Job {
 			throw new Exception( $status->getWikiText() );
 		}
 		$logId = $status->value;
-		$dbw = wfGetDB( DB_MASTER );
+		$dbw = wfGetDB( DB_PRIMARY );
 		// Try and make it run after the rc entry insert.
 		// FIXME doesn't work.
 		/* DeferredUpdates::addCallableUpdate( function () use ( $dbw, $logId, $pageId ) {
@@ -99,7 +99,7 @@ class SyncArticleJob extends Job {
 		} else {
 			$localPage = WikiPage::factory( $localTitle );
 		}
-		$dbw = wfGetDB( DB_MASTER );
+		$dbw = wfGetDB( DB_PRIMARY );
 		foreach ( $toImport as $info ) {
 			$user = User::newFromName( $info->rev_user_text, false );
 			$content = ContentHandler::makeContent(
@@ -219,7 +219,7 @@ class SyncArticleJob extends Job {
 	}
 
 	private function getMostRecentForeignRev( Title $localTitle ) {
-		$dbr = wfGetDB( DB_MASTER );
+		$dbr = wfGetDB( DB_PRIMARY );
 		return $dbr->selectField(
 			[ 'revision', 'foreignrevisionlink' ],
 			'frl_foreign_rev_id',
